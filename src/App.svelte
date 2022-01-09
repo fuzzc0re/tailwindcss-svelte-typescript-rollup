3c228536699bbbfd307f66d4165a7db6d76b8e2c
fuzzc0re <dim@karoukis.com>
2022-01-09 11:55:08 +0200

    Everything works out of the box

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Router from 'svelte-spa-router';
+  import { wrap } from 'svelte-spa-router/wrap';
+
+  import Header from './components/Header.svelte';
+
+  import Home from './routes/Home.svelte';
+  import NotFound from './routes/NotFound.svelte';
+
+  const routes = {
+    // Exact path
+    '/': Home,
+
+    '/about': wrap({
+      asyncComponent: () => import('./routes/About.svelte'),
+    }),
+
+    // Using named parameters, with last being optional
+    /* '/author/:first/:last?': Author, */
+
+    // Wildcard parameter
+    /* '/book/*': Book, */
+
+    // Catch-all
+    // This is optional, but if present it must be the last
+    '*': NotFound,
+  };
+</script>
+
+<Header />
+
+<Router routes="{routes}" restoreScrollState="{true}" />
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { link } from 'svelte-spa-router';
+</script>
+
+<footer class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+  <a class="text-gray-500 no-underline hover:no-underline" href="/copyright" use:link>&copy; Company 2022</a>
+</footer>
